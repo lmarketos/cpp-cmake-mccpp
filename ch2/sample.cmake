058da2ad53fae9b7d6b656bac84ed0ccfd6b3495
LM <git@marketos.com>
2023-10-07 23:00:15 -0600

    more variable info

--- ch2/sample.cmake
+++ ch2/sample.cmake
@@ -27,3 +27,33 @@ message("Quoted argument
  with newline \n and escaped slash \\ and quote \"; also
  variable substitution; Version: ${CMAKE_VERSION}")
 
+# Unquoted arguments have to escape spaces and ;'s
+message(This\ is\ one\ argument)
+message(This is three)
+message(This;is;three)
+
+# assign to variables using set/unset
+# access via ${}
+set(test value)
+message(${test})
+
+set([[bad but valid name]] value)
+message(${bad\ but\ valid\ name})
+
+# nested var references are evaluated inside out
+set(newvalue "This is the value")
+set(var2 value)
+message(${new${var2}})
+
+# envirionment vars accessed with $ENV{}
+#message($ENV{HOSTNAME})
+
+# lists are variables with multiple values
+set(mylist this is a list)
+message(${mylist})
+set(mylist this is "also;a;list")
+message(${mylist})
+
+list(LENGTH mylist mylength)
+
+message(${mylength})
